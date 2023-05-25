611aa1e3ac6958dfd6d51ca9689e0f754e6e2b94
alexeyb7 <alexeyb7@gmail.com>
2023-05-26 01:44:03 +0300

    Add DZ1_2,1_4,1_6

--- dz1/dz1_2.py
+++ dz1/dz1_2.py
@@ -1,9 +1,15 @@
 # Задача 2: Найдите сумму цифр трехзначного числа.
 in_str = input("Введите трехзначное число: ")
 in_digit = int(in_str)
-if (in_digit < 100) or (in_digit > 1000):
+if (in_digit < 100) or (in_digit >= 1000):
     print("Повторите ввод")
 else:
+    summ = 0
+    while in_digit:
+        summ += in_digit % 10
+        in_digit //= 10
+    print("Сумма цифр числа:", summ)
+    # второй вариант
     out_digit1 = in_str[0]
     out_digit2 = in_str[1]
     out_digit3 = in_str[2]
