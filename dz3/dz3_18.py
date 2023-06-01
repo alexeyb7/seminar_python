80ab8280b5eef243a2b7bc96b4b01d00c2bbd99f
alexeyb7 <alexeyb7@gmail.com>
2023-06-02 01:03:15 +0300

    Modified DZ3_18

--- dz3/dz3_18.py
+++ dz3/dz3_18.py
@@ -14,12 +14,15 @@ x = int(input("Введите число: "))
 if num.count(x) != 0:
     print("Число", x, " в массиве встречается ", num.count(x), " раз")
 else:
-    #    i = 0
-    #    while i < number - 1:
-    #        if (abs(x - num[i])) > (abs(x - num[i + 1])):
-    #            near = num[i + 1]
-    #        near = num[i]
-    #        i += 1
-    near = min([abs(x - num[i])])
+    i = 0
+    t = abs(x - num[i])
+    while i < number - 1:
+        if t > (abs(x - num[i + 1])):
+            near = num[i + 1]
+            t = abs(x - num[i + 1])
+        elif t > (abs(x - num[i])):
+            near = num[i]
+            t = abs(x - num[i])
+        i += 1
 
 print("Ближайший к числу элемент массива", near)
