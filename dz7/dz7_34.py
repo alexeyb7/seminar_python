c97bede9821110e7af35721794b1c05817fedf4b
alexeyb7 <alexeyb7@gmail.com>
2023-06-20 21:03:49 +0300

    Modify 7_34

--- dz7/dz7_34.py
+++ dz7/dz7_34.py
@@ -12,9 +12,11 @@
 """
 
 
-print(
-    ("Пам парам", "Парам пам-пам")[
-        len(s := set(sum(j in "уеыаоэяиюё" for j in i) for i in input().split())) == 1
-        and s.pop() != 0
-    ]
-)
+c = input("Введите стих: ")
+st = c.lower().split()
+f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
+t = f(st[0])
+if all([f(i) == t for i in st]):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
